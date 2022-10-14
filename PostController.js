d1703c07bfb39965bfc8351e0177b7333425aadf
Hamafac <borysenko777@gmail.com>
2022-10-14 18:00:56 +0300

    refactoring PostController add PostService

--- PostController.js
+++ PostController.js
@@ -1,61 +1,49 @@
 import Post from "./Post.js"
+import PostService from "./PostService.js"
 
 class PostController {
    async create(req, res) {
       try {
-         const { author, title, content, picture } = req.body
-         const post = await Post.create({ author, title, content, picture })
+         const post = await PostService.create(req.body)
          res.json(post)
       } catch (error) {
-         res.status(500).json(error)
+         res.status(500).json(error.message)
       }
    }
 
    async getAll(req, res) {
       try {
-         const posts = await Post.find();
+         const posts = await PostService.getAll();
          return res.json(posts);
       } catch (error) {
-         res.status(500).json(error)
+         res.status(500).json(error.message)
       }
    }
 
    async getOne(req, res) {
       try {
-         const { id } = req.params
-         if (!id) {
-            res.status(400).json({ message: 'id not found' })
-         }
-         const post = await Post.findById(id);
+         const post = await PostService.getOne(req.params.id)
          return res.json(post);
       } catch (error) {
-         res.status(500).json(error)
+         res.status(500).json(error.message)
       }
    }
 
    async update(req, res) {
       try {
-         const post = req.body
-         if (!post._id) {
-            res.status(400).json({ message: 'id not found' })
-         }
-         const updatedPost = await Post.findByIdAndUpdate(post._id, post, { new: true });
+         const updatedPost = await PostService.update(req.body)
          return res.json(updatedPost);
       } catch (error) {
-         res.status(500).json(error)
+         res.status(500).json(error.message)
       }
    }
 
    async delete(req, res) {
       try {
-         const { id } = req.params
-         if (!id) {
-            res.status(400).json({ message: 'id not found' })
-         }
-         const post = await Post.findByIdAndDelete(id);
+         const post = await PostService.delete(req.params.id)
          return res.json(post);
       } catch (error) {
-         res.status(500).json(error)
+         res.status(500).json(error.message)
       }
    }
 }
